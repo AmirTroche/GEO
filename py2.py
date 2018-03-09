8c16da1163e2bc620d7461077190f4da010f30b8
vilma nilsson <vilmaullstorp@live.se>
2018-03-09 13:46:53 +0100

    hej
    hej

---
+++ py2.py
@@ -0,0 +1,5 @@
+# -*- coding: cp1252 -*-
+print ("Välkommen till detta program som beräknar hur många sekunder det går på x antal timmar")
+hours = int(input("Antal timmar: "))
+result = hours*60*60
+print ("På ", hours, " timme/timmar går det ", str(result), " sekunder")
