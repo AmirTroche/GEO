ab0e7e90aec0baeea4c829549a138242533dc694
vilma nilsson <vilmaullstorp@live.se>
2018-03-09 13:48:46 +0100

    fixat bla
    blhg

---
+++ modul4_2.py
@@ -0,0 +1,44 @@
+'''phone_book = {}
+
+phone_book["Vilma"] = "101-0000"
+phone_book["Ida"] = "101-1111"
+phone_book["Zacke"] = "101-2222"
+
+for name in phone_book:
+    print(name + ":" + phone_book[name])
+'''
+
+'''
+
+eng_to_swe = {}
+
+eng_to_swe["You"] = "Du"
+eng_to_swe["are"] = "är"
+eng_to_swe["weird"] = "gån"
+for name in eng_to_swe:
+    print(eng_to_swe["you", "are", "weird"])
+
+for i in eng_to_swe:
+    print((eng_to_swe["You", "are", "weird"]))
+'''
+
+
+def print_lex(eng_swe):
+    print(eng_swe["you"], eng_swe["are"], eng_swe["weird"])
+
+def print_lexi():
+    print(print_lex(eng_swe))
+
+eng_swe = []
+
+eng_swe["you"] = "du"
+eng_swe["are"] = "är"
+eng_swe["weird"] = "gån"
+
+print_lex(eng_swe)
+print_lexi()
+
+
+
+
+
