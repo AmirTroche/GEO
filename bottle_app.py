c1f4186e66301e16248d06d32dd9cd138e75c36e
Viktoriya Khegay <viktoriya.khegay@gmail.com>
2018-04-25 14:03:12 +0200

    New version

---
+++ bottle_app.py
@@ -0,0 +1,57 @@
+from bottle import run, route, template, request
+import quiz
+
+""" Hämtar Main & Fakta sidorna"""
+
+@route('/')
+def main():
+    return template('main')
+
+@route('/north_facts')
+def get_north_facts():
+    return template('north_facts')
+
+@route('/east_facts')
+def get_east_facts():
+    return template('east_facts')
+
+@route('/west_facts')
+def get_west_facts():
+    return template('west_facts')
+
+@route('/south_facts')
+def get_south_facts():
+    return template('south_facts')
+
+""" Hämtar Quiz"""
+
+@route('/quiz_north')
+def get_quiz_north():
+    questions = quiz.get_quiz()
+    return template('quiz_north', questions = questions)
+
+@route('/quiz_east')
+def get_quiz_east():
+    return template('quiz_east')
+
+@route('/quiz_west')
+def get_quiz_west():
+    return template('quiz_west')
+
+@route('/quiz_south')
+def get_quiz_south():
+    return template('quiz_south')
+
+""" Hämtar kontaktsidan"""
+
+@route('/contact')
+def contact():
+    return template('contact')
+
+
+@route('/test')
+def test():
+    answer = request.query['answer']
+    print(answer)
+
+run(host='localhost', port=8080)
--- readme.md
+++ readme.md
@@ -1,1 +1,30 @@
-# YourGEO
+YourGEO
+
+----How to open our product----
+
+1. download our code from our repository.
+
+2. Open the map YourGeo Webb
+
+3. Then select view to get the html code
+
+4. Choose any site to start with.
+
+
+
+-----How to open our product code----
+
+1. Open any editor
+
+2. Press "arkiv" and choose to select open map.
+
+3. Choose our map "YourGeo Webb"
+
+4. When all the maps pops up in your editor, Choose either View or Static to look at the Html and Css.
+
+
+
+Thanks! 
+
+Team YourGeo
+
