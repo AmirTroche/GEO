e32658b4068930a2b8c3eab1b146f5945b656f11
vilma nilsson <vilmaullstorp@live.se>
2018-05-15 21:50:16 +0200

    Lagt till kod för varje område (förutom north)
    Nu är jag duktig och skriver en summary :DD::D:D:D

--- bottle_app.py
+++ bottle_app.py
@@ -1,7 +1,7 @@
 from bottle import run, route, template, request, static_file
 import quiz
 
-"""Returns main-page and fact- pages"""
+""" Hamtar Main & Fakta sidorna"""
 
 @route('/')
 def main():
@@ -23,39 +23,55 @@ def get_west_facts():
 def get_south_facts():
     return template('south_facts')
 
-"""Returns quiz- pages"""
+""" Hamtar Quiz"""
 
 @route('/north_quiz')
 def get_north_quiz():
     north=quiz.get_quiz_north()
+    
     return template('north_quiz',questions=north)
+
+    
+
 
 @route('/east_quiz')
 def get_east_quiz():
-    return template('east_quiz')
+    east=quiz.get_quiz_north()
+    
+    return template('east_quiz', questions=east)
 
 @route('/west_quiz')
 def get_west_quiz():
-    return template('west_quiz')
+    west=quiz.get_quiz_west()
+    
+    return template('west_quiz', questions=west)
 
 @route('/south_quiz')
 def get_south_quiz():
-    return template('south_quiz')
+    south=quiz.get_quiz_south()
+    
+    return template('south_quiz', questions=south)
 
-"""Returns contact- page"""
+
+""" Hamtar kontaktsidan"""
 
 @route('/contact')
 def contact():
     return template('contact')
 
 
+@route('/test')
+def test():
+    answer = request.query['answer']
+    print(answer)
+
 @route("/static/<filename:path>")
 def server_static(filename):
-    """Handles the routes to our static files
+    '''Handles the routes to our static files
 
     Returns:
             file : the static file requested by URL
-    """
+    '''
     return static_file(filename, root="static")
 
 run(host='localhost', port=8050, debug=True, reloader=True)
--- quiz.py
+++ quiz.py
@@ -2,29 +2,70 @@ import pymysql
 
 
 def connectDB():
-    """Connects to database"""
+    # Ansluter till vår databas
     db = pymysql.connect(host="localhost",
                          user="root",
                          passwd="",
                          db="yourgeo",
                          cursorclass=pymysql.cursors.DictCursor)
     
-    """Creates a cursor for the database"""
+    # Skapar en pekare mot databasen
     cursor = db.cursor()
     return db, cursor
                       
-
+#North
 def get_quiz_north():
     db, cursor = connectDB()
-    """Sends a question to get all questions from the table "questions"""
+    # Skickar iväg en fråga för att hämta alla frågor från tabellen "questions"
     sql = "SELECT * from answers join questions on questions.questionID=answers.answersID WHERE Area='{}'".format("Norra Europa")
     
     cursor.execute(sql)
-    """Recieves answer, saves it in the variable "north"""
+    # Tar emot svaret, sparar det i variabeln "north"
     north = cursor.fetchall()
     return north
 
+#East
+def get_quiz_east():
+    db, cursor = connectDB()
+    # Skickar iväg en fråga för att hämta alla frågor från tabellen "questions"
+    sql = "SELECT * from answers join questions on questions.questionID=answers.answersID WHERE Area='{}'".format("Östra Europa")
+    
+    cursor.execute(sql)
+    # Tar emot svaret, sparar det i variabeln "east"
+    east = cursor.fetchall()
+    return east
 
+#West
+def get_quiz_west():
+    db, cursor = connectDB()
+    # Skickar iväg en fråga för att hämta alla frågor från tabellen "questions"
+    sql = "SELECT * from answers join questions on questions.questionID=answers.answersID WHERE Area='{}'".format("Västra Europa")
+    
+    cursor.execute(sql)
+    # Tar emot svaret, sparar det i variabeln "west"
+    west = cursor.fetchall()
+    return west
+
+#South
+def get_quiz_south():
+    db, cursor = connectDB()
+    # Skickar iväg en fråga för att hämta alla frågor från tabellen "questions"
+    sql = "SELECT * from answers join questions on questions.questionID=answers.answersID WHERE Area='{}'".format("South Europa")
+    
+    cursor.execute(sql)
+    # Tar emot svaret, sparar det i variabeln "south"
+    south = cursor.fetchall()
+    return south
+
+'''def get_answers_north():
+    db, cursor = connectDB()
+    # Skickar iväg en fråga för att hämta alla svar från tabellen "answers"
+    sql = "SELECT * from answers join questions on questions.questionID=answers.answersID WHERE Area='{}'".format("Norra Europa")
+    cursor.execute(sql)
+    # Tar emot svaret, sparar det i variabeln "north"
+    north_answers = cursor.fetchall()
+    print(north_answers)
+    return north_answers'''
 
 
 
